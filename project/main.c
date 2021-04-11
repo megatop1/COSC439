9e808277ada0487eaab708224c2e567c498ecd9f
megatop1 <beachbumrj@optonline.net>
2021-04-10 23:20:17 -0700

    Read/Write to device

--- project/main.c
+++ project/main.c
@@ -33,30 +33,34 @@ int init_module(void) { //check if a device has been registered properly and pri
 	}
         //If does not fail just continue 
 	printk(KERN_ALERT "I was assigned a major number %d", Major);
-	printk(KERN_ALERT "Please create a device with name \n mknod /dev/%s c %d 0", DEVICE_NAME, Major); //When we create our device and try to interact with it, the driver will wait for input for a read/write operation. mknod is how you create a device. It will grab our device name of root_kit
+	printk(KERN_ALERT "Please create a device with name \n mknod /dev/%s c %d 0 \n", DEVICE_NAME, Major); //When we create our device and try to interact with it, the driver will wait for input for a read/write operation. mknod is how you create a device. It will grab our device name of root_kit
 	return 0;
 }
 
-//Open Driver Function
+//Open Driver Function, ran when syscall_open is called, it run this function
 int device_open(struct inode * inode, struct file *file) {
-	static int counter = 0;
+	//static int counter = 0;
 	if(Device_Open) { //if the device is open return that the device is busy
 		return -EBUSY;
 	}
 
 	//Copy a message into a buffer that will be printed to the screen
-	sprintf(msg, "I was opened %d times", counter++);
+	//sprintf(msg, "I was opened %d times", counter++);
 	msg_Ptr = msg;
 	try_module_get(THIS_MODULE);
 	return 0;
 }
 
+/* Called when a process closes the device file */
 int device_release(struct inode * inode, struct file *file) { //ensures when device is done reading then it will release the device so we can use it again. Similar to close() 
 	Device_Open--; //Make it ready to the user next caller
 	module_put(THIS_MODULE);
 	return 0;
 }
 
+/* Called when a process, which already opened the dev file, attempts to
+   read from it. Function runs when syscall_read is ran
+*/
 ssize_t device_read(struct file * file, char * buffer, size_t length, loff_t *offset) {
 	int bytes_read = 0;
 	if(*msg_Ptr == 0) { //if the message is nothing, do nothing and return nothing
@@ -74,6 +78,14 @@ ssize_t device_read(struct file * file, char * buffer, size_t length, loff_t *of
 }
 
 ssize_t device_write(struct file * file, const char * buffer, size_t length, loff_t *offset) {
-	printk(KERN_ALERT "I don nothing!\n");
-	return 0;
+	int count = 0;
+	memset(msg, 0, BUF_LEN); //clear our buffer then use msg as the buffer which is whats getting read to, then set it to the size of that buffer using buffer length
+
+	while(length > 0) {
+		copy_from_user(msg, buffer, BUF_LEN-1); //copy to the msg buffer and use buffer length - 1 so we don't overflow
+		length--;
+		msg[BUF_LEN-1] = 0x00;
+	}
+
+	return count; /*Always return something!*/
 }
